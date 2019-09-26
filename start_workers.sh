fa920baff5cce08db3cf564d6ed839409376ab2d
breadbread1984 <breadbread1984@163.com>
2019-09-26 11:09:23 +0800

    update start celery script

--- start_workers.sh
+++ start_workers.sh
@@ -6,4 +6,4 @@ celery -A query worker --loglevel=INFO --concurrency=1 -n worker2@%h &
 celery -A query worker --loglevel=INFO --concurrency=1 -n worker3@%h &
 celery -A query worker --loglevel=INFO --concurrency=1 -n worker4@%h &
 # checkout worker status
-celery -A worker status
+celery -A query status
