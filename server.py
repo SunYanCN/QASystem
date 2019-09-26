2d725f5192ec3e47bff1e6c50fcc2efc67413982
breadbread1984 <breadbread1984@163.com>
2019-09-26 13:50:05 +0800

    enable cross origin for both flask and socketio

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 from flask import Flask, request, jsonify, session;
-from flask_cors import CORS;
 from flask_socketio import SocketIO, join_room;
 from uuid import uuid4;
 from query import query;
 
 app = Flask(__name__);
 app.secret_key = 'swj*2019!'; # secret key for using session
-#cors = CORS(app, resources = {r"/*":{"origins":"*"}});
 socketio = SocketIO(app, message_queue = 'amqp://guest:guest@localhost:5672');
 
 @app.route('/')
@@ -36,7 +34,7 @@ def after_request(response):
 @socketio.on('connect')
 def socket_connect():
     # nothing need to be done when client connect with server
-    pass;
+    print('connected to socket!');
 
 @socketio.on('join_room', namespace = '/query')
 def on_room():
@@ -46,4 +44,4 @@ def on_room():
 
 if __name__ == "__main__":
 
-    socketio.run(app, host = '192.168.1.102', port = 5000);
+    socketio.run(app, host = '192.168.1.102', port = 5000, cors_allowed_origins="*");
