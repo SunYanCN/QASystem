85cf08a9600fa93da75a9b01af5c68e615c35e26
breadbread1984 <breadbread1984@163.com>
2019-09-30 09:55:53 +0800

    save logs and pids into directories

--- start_workers.sh
+++ start_workers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+rm -rf logs && mkdir logs
+rm -rf pids && mkdir pids
 # start workers
-celery -A query worker --loglevel=INFO --concurrency=1 -n worker1@%h &
-celery -A query worker --loglevel=INFO --concurrency=1 -n worker2@%h &
-celery -A query worker --loglevel=INFO --concurrency=1 -n worker3@%h &
-celery -A query worker --loglevel=INFO --concurrency=1 -n worker4@%h &
+celery -A query worker --loglevel=INFO --concurrency=1 -n worker1@%h --logfile=logs/%n.log --pidfile=pids/%n.pid &
+celery -A query worker --loglevel=INFO --concurrency=1 -n worker2@%h --logfile=logs/%n.log --pidfile=pids/%n.pid &
+celery -A query worker --loglevel=INFO --concurrency=1 -n worker3@%h --logfile=logs/%n.log --pidfile=pids/%n.pid &
+celery -A query worker --loglevel=INFO --concurrency=1 -n worker4@%h --logfile=logs/%n.log --pidfile=pids/%n.pid &
 # checkout worker status
 celery -A query status
+
